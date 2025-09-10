32b0e18a6d50b0e7f494abe1c6b88f8dbc09d37f
Saad <saadbeenco@gmail.com>
2025-09-10 14:28:14 +0500

    feat: integrate shadcn UI components and update styling system

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,16 +1,13 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Poppins as FontPoppins } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
+const fontPoppins = FontPoppins({
+  variable: "--font-poppins",
   subsets: ["latin"],
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
 });
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -25,7 +22,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${fontPoppins.variable} font-sans antialiased`}
       >
         {children}
       </body>
